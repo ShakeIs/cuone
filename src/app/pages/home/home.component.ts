8745df04255c5725195dfc67f3362ef52782c7e2
Laurynas Šeikis <laurynas.seikis@twoday.com>
2026-01-19 19:47:29 +0200

    More fixes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -37,15 +37,15 @@ export class Home {
   ];
 
   mobileImages = [
-    {normal: 'public/clients/gateris.svg', hover: 'public/clients/gateris-hover.svg'},
-    {normal: 'public/clients/kunset.svg', hover: 'public/clients/kunset-hover.svg'},
-    {normal: 'public/clients/kupiskio.svg', hover: 'public/clients/kupiskio-hover.svg'},
-    {normal: 'public/clients/vaisiu.svg', hover: 'public/clients/vaisiu-hover.svg'},
-    {normal: 'public/clients/artele.svg', hover: 'public/clients/artele-hover.svg'},
-    {normal: 'public/clients/hope-hover.svg', hover: 'public/clients/hope-hover.svg'},
-    {normal: 'public/clients/dublis.svg', hover: 'public/clients/dublis-hover.svg'},
-    {normal: 'public/clients/skin.svg', hover: 'public/clients/skin-hover.svg'},
-    {normal: 'public/clients/lineka.svg', hover: 'public/clients/lineka-hover.svg'},
+    {normal: 'public/clients/gateris.svg', hover: 'public/clients/gateris-hover.svg', width: 80},
+    {normal: 'public/clients/kunset.svg', hover: 'public/clients/kunset-hover.svg', width: 80},
+    {normal: 'public/clients/kupiskio.svg', hover: 'public/clients/kupiskio-hover.svg', width: 80},
+    {normal: 'public/clients/vaisiu.svg', hover: 'public/clients/vaisiu-hover.svg', width: 80},
+    {normal: 'public/clients/artele.svg', hover: 'public/clients/artele-hover.svg', width: 80},
+    {normal: 'public/clients/hope-hover.svg', hover: 'public/clients/hope-hover.svg', width: 55},
+    {normal: 'public/clients/dublis.svg', hover: 'public/clients/dublis-hover.svg', width: 80},
+    {normal: 'public/clients/skin.svg', hover: 'public/clients/skin-hover.svg', width: 80},
+    {normal: 'public/clients/lineka.svg', hover: 'public/clients/lineka-hover.svg', width: 80},
   ];
 
 }
